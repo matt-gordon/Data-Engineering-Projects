68c5a70ec904e7c53984a5f246b7e9ab43697043
Matt Gordon <matt.gordon82@gmail.com>
2020-02-11 19:55:16 +1100

    applied NOT NULL constraints and allowed update of user level

--- Project 1/scripts/sql_queries.py
+++ Project 1/scripts/sql_queries.py
@@ -18,21 +18,21 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id serial PRIMARY KEY, user_id int, 
-                        song_id varchar, artist_id varchar, session_id int, start_time timestamp, level varchar, 
-                        location varchar, user_agent varchar);""")
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id serial PRIMARY KEY, user_id int NOT NULL, 
+                        song_id varchar NOT NULL, artist_id varchar NOT NULL, session_id int NOT NULL, start_time timestamp NOT NULL, level varchar NOT NULL, 
+                        location varchar NOT NULL, user_agent varchar NOT NULL);""")
 
-user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int PRIMARY KEY, first_name varchar, 
-                    last_name varchar, gender char, level varchar);""")
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int PRIMARY KEY, first_name varchar NOT NULL, 
+                    last_name varchar NOT NULL, gender char NOT NULL, level varchar NOT NULL);""")
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY, artist_id varchar, title varchar, 
-                    year int, duration numeric);""")
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY, artist_id varchar NOT NULL, title varchar NOT NULL, 
+                    year int NOT NULL, duration numeric NOT NULL);""")
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY, artist_name varchar, artist_location varchar, 
-                    artist_latitude numeric, artist_longitude numeric);""")
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY, artist_name varchar NOT NULL, artist_location varchar NOT NULL, 
+                    artist_latitude numeric NOT NULL, artist_longitude numeric NOT NULL);""")
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time timestamp PRIMARY KEY, hour int, day int, week int, 
-                    month int, year int, weekday varchar);""")
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time timestamp PRIMARY KEY, hour int NOT NULL, day int NOT NULL, week int NOT NULL, 
+                    month int NOT NULL, year int NOT NULL, weekday varchar NOT NULL);""")
 
 # INSERT RECORDS
 
@@ -41,7 +41,7 @@ songplay_table_insert = ("""INSERT INTO songplays (user_id, song_id, artist_id, 
 
 user_table_insert = ("""INSERT INTO  users (user_id, first_name, last_name, gender, level)
                     VALUES (%s,%s,%s,%s,%s)
-                    ON CONFLICT  (user_id) DO NOTHING;""")
+                    ON CONFLICT  (user_id) DO UPDATE SET level = EXCLUDED.level;""")
 
 song_table_insert = ("""INSERT INTO songs (song_id, artist_id, title, year, duration)
                     VALUES (%s,%s,%s,%s,%s)

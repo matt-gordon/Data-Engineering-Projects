8175e3269302b4f7f8d38efddd8e34935da92cd6
Matt Gordon <matt.gordon82@gmail.com>
2020-03-09 18:37:39 +1100

    added Docstring

--- Project 3/create_tables.py
+++ Project 3/create_tables.py
@@ -1,15 +1,54 @@
+"""Drops tables if they exist and then creates new tables as defined in sql_queries.py
+
+Program establishes a connection to the running Redshift cluster using parameters defined
+in dwh.cfg.  To ensure clean, empty tables are used all required Sparkify analytics tables
+are dropped if they exist and then new tables are created.
+
+    Typical usage:
+        python create_tables.py
+    
+    Note: This must be run before etl.py to ensure tables exist and are empty.
+"""
 import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """Drops all Sparkify staging, fact and dimension tables from Redshift cluster.
+
+    Executes all drop table statements defined in sql_queries.py
+
+    Args:
+        cur: established cursor to execute commands/queries on the redshift cluster
+        conn: established redshift database connection
+    
+    Returns:
+        Undefined.  
+
+    Raises:
+        Undefined.  
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """Creates all Sparkify staging, fact and dimension tables on Redshift cluster.
+
+    Executes all create table statements defined in sql_queries.py
+
+    Args:
+        cur: established cursor to execute commands/queries on the redshift cluster
+        conn: established redshift database connection
+    
+    Returns:
+        Undefined.  
+
+    Raises:
+        Undefined.  
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
@@ -27,7 +66,6 @@ def main():
     print("Tables created...")
 
     conn.close()
-
 
 if __name__ == "__main__":
     main()
